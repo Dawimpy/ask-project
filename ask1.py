e3b91cf026d73950712a8b9c8718c56953f023b4
Anonymous1231-dev <87801676+Anonymous1231-dev@users.noreply.github.com>
2023-04-06 01:23:37 +0000

    skelton1

--- ask1.py
+++ ask1.py
@@ -1,1 +1,15 @@
+#Atur cara pengiraan untungan bazar Ramadan
 
+print("***ATUR CARA PENGIRAAN KEUNTUNGAN HARIAN BAZAR RAMADAN***")
+
+#Hasil jualan
+h_jualan=float(input("Sila masuk hasil jualan harian:RM "))
+
+#Kos untuk membeli barangan bagi gerai bazar
+kos_barangan=float(input("Sila mausk kos barangan:RM "))
+
+#kira untungan bersih
+u_bersih=float(h_jualan-kos_barangan)
+print("Untungan ialah RM",u_bersih)
+
+#kira peratusan untungan
