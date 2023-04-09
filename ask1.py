85afd2274593d1fb90236b1ef6622b08330d2013
Anonymous1231-dev <87801676+Anonymous1231-dev@users.noreply.github.com>
2023-04-09 12:39:57 +0800

    Change some variable

--- ask1.py
+++ ask1.py
@@ -3,13 +3,13 @@
 print("***ATUR CARA PENGIRAAN KEUNTUNGAN HARIAN BAZAR RAMADAN***")
 
 #Hasil jualan
-h_jualan=float(input("Sila masuk hasil jualan harian:RM "))
+h_harian=float(input("Sila masuk hasil jualan harian:RM "))
 
 #Kos untuk membeli barangan bagi gerai bazar
 kos_barangan=float(input("Sila mausk kos barangan:RM "))
 
 #kira untungan bersih
-u_bersih=float(h_jualan-kos_barangan)
+u_bersih=float(h_harian-kos_barangan)
 print("Untungan ialah RM",u_bersih)
 
 #kira peratusan untungan bersih
